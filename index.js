05a8b107a85c7635bce3baf5027032e80f832a06
David Levi <dave@double-apps.com>
2016-02-13 13:40:54 -0500

    Maintain APN connection per dpd-apn resource instance, rather than creating new connection for every notification. Beginning of handling APN feedback.

--- index.js
+++ index.js
@@ -2,20 +2,57 @@ var Resource = require('deployd/lib/resource');
 var util     = require('util');
 var apn      = require('apn');
 
-function dpdapn(options){
-
+function dpdapn(instanceName){
     Resource.apply(this, arguments);
 
     var options = {
         "cert":       this.config.cert,
         "key":        this.config.key,
-        "passphrase": this.config.passphrase
+        "passphrase": this.config.passphrase,
+        "production": !this.config.sandbox,
+        "contentAvailable": this.config.contentAvailable
     }
 
-    this.apnconn = new apn.Connection(options);
+    var needNewConnection = false;
+    if (dpdapn.connectionOptions[instanceName]) {
+        if (JSON.stringify(options) !== JSON.stringify(dpdapn.connectionOptions[instanceName])) {
+            needNewConnection = true;
+        }
+    } else {
+        needNewConnection = true;
+    }
+
+    if (needNewConnection) {
+        dpdapn.connections[instanceName] = new apn.Connection(options);
+        dpdapn.connectionOptions = options;
+    }
+
+/*
+    if (!dpdapn.feedback[instanceName] && this.config.cert && this.config.key) {
+        dpdapn.feedback[instanceName] = new apn.Feedback(
+            {
+                cert: this.config.cert,
+                key:  this.config.key,
+                batchFeedback: true,
+                interval: 10,
+                address: this.config.sandbox ? "feedback.sandbox.push.apple.com" : "feedback.push.apple.com"
+            }
+        );
+        dpdapn.feedback[instanceName].on("feedback", function(devices) {
+*/
+//console.log(devices);
+//dpd.devices.get(function(result,error) {
+//  console.log(result);
+//});
+        });
+    }
 }
 
 util.inherits(dpdapn, Resource);
+
+dpdapn.connections = {};
+dpdapn.feedback = {};
+dpdapn.connectionOptions = {};
 
 dpdapn.label = "APN";
 dpdapn.events = ["post"];
@@ -53,34 +90,56 @@ dpdapn.basicDashboard = {
             name        : 'sound',
             type        : 'string',
             description : 'Sound file. Defaults to "ping.aiff"'
+        },
+        {
+            name        : 'sandbox',
+            type        : 'checkbox',
+            description : 'Use production or sandbox APN service. Defaults to production.'
+        },
+        {
+            name        : 'contentAvailable',
+            type        : 'checkbox',
+            description : 'Should push notifications use content-available. Defaults to false.'
         }
     ]
 }
 
 dpdapn.prototype.handle = function ( ctx, next ) {
-
-    var device = "";
+//dpd.devices.get(function(result,error) {
+//  console.log(result);
+//});
+    var instanceName = ctx.req.url.replace(/\//, "");
+    var apnconn = dpdapn.connections[instanceName];
+    var devices = [];
     var message = "";
 
-    if(ctx.body && ctx.body.device){
-        device = ctx.body.device;
+    if(ctx.body && ctx.body.devices){
+        devices = ctx.body.devices;
     }
+devices.push("ab06e720b95a03e459f72ee5c830801e3ef19f4ed07732bb85b2cadcf1feea48");
 
     if(ctx.body && ctx.body.message){
         message = ctx.body.message;
     }else{
-        message = this.config.defaultMsg ? this.config.defaultMsg : "New message";
+        message = this.config.defaultMsg ? this.config.defaultMsg : "";
     }
 
-    var myDevice = new apn.Device(device);
     var note = new apn.Notification();
 
     note.expiry = Math.floor(Date.now() / 1000) + 360000;
-    note.badge = this.config.badge ? parseInt(this.config.badge) : 1;
-    note.sound = this.config.sound ? this.config.sound : "ping.aiff";
-    note.alert = message;
+    if (this.config.badge) note.badge = parseInt(this.config.badge);
+    if (this.config.sound) note.sound = this.config.sound;
+    if (message != "") note.alert = message;
+    if (ctx.body.payload) note.payload = ctx.body.payload;
+    if (this.config.contentAvailable) note.setContentAvailable(true);
 
-    this.apnconn.pushNotification(note, myDevice);
+    try {
+      apnconn.pushNotification(note, devices);
+      ctx.done(null, {dispatched: true});
+    } catch (err) {
+      ctx.done(err);
+    }
+
 
 }
 
